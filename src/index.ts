702b4b363688d975adc8da6221003b8bf6bc3b5b
Mikk Küttim <kuttim.mikk@gmail.com>
2023-06-25 03:51:47 +0300

    feat: Add Git repository check and user confirmation before overwriting hooks
    This commit introduces two new features:
    
    1. The install function now checks if the target directory is a valid Git repository before attempting to install hooks. This prevents errors that would occur if the script was run in a directory that is not a Git repository.
    
    2. If a hook file already exists in the target repository, the user is asked for confirmation before the existing hook is overwritten. This prevents accidental loss of existing hooks.
    
    These changes make the script more robust and user-friendly.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,54 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as readline from 'readline';
 
-function install(targetDir: string) {
-  const sourceDir = path.join(__dirname, 'hooks');
-  const targetHookDir = path.join(targetDir, '.git', 'hooks');
+async function askQuestion(query: string): Promise<boolean> {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-  if (!fs.existsSync(sourceDir)) {
-    console.error(`Error: Could not find source hooks directory at ${sourceDir}`);
-    return;
-  }
+    return new Promise(resolve =>
+        rl.question(query, ans => {
+            rl.close();
+            resolve(ans.toLowerCase() === 'y');
+        })
+    );
+}
 
-  if (!fs.existsSync(targetHookDir)) {
-    console.error(`Error: Could not find target hooks directory at ${targetHookDir}`);
-    return;
-  }
+async function install(targetDir: string) {
+    const sourceDir = path.join(__dirname, 'hooks');
+    const gitDir = path.join(targetDir, '.git');
+    const targetHookDir = path.join(gitDir, 'hooks');
 
-  fs.readdirSync(sourceDir).forEach((file) => {
-    const sourceFile = path.join(sourceDir, file);
-    const targetFile = path.join(targetHookDir, file);
-    fs.copyFileSync(sourceFile, targetFile);
-    fs.chmodSync(targetFile, '755');
-  });
+    if (!fs.existsSync(sourceDir)) {
+        console.error(`Error: Could not find source hooks directory at ${sourceDir}`);
+        return;
+    }
 
-  console.log(`Installed git hooks in ${targetDir}`);
+    if (!fs.existsSync(gitDir) || !fs.lstatSync(gitDir).isDirectory()) {
+        console.error(`Error: Target directory is not a Git repository: ${targetDir}`);
+        return;
+    }
+
+    fs.readdirSync(sourceDir).forEach(async (file) => {
+        const sourceFile = path.join(sourceDir, file);
+        const targetFile = path.join(targetHookDir, file);
+
+        if (fs.existsSync(targetFile)) {
+            console.log(`Warning: Hook already exists: ${file}`);
+            const overwrite = await askQuestion('Do you want to overwrite it? (y/n): ');
+            if (!overwrite) {
+                console.log(`Skipping: ${file}`);
+                return;
+            }
+        }
+
+        fs.copyFileSync(sourceFile, targetFile);
+        fs.chmodSync(targetFile, '755');
+    });
+
+    console.log(`Installed git hooks in ${targetDir}`);
 }
 
 export { install };
